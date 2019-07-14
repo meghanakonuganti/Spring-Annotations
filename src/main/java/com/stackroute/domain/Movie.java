54faedbfe88094f3fdb0dc20068ab693b56279a6
meghanakonuganti <meghanaraj1016@gmail.com>
2019-07-14 22:12:16 +0530

    Annotations Task3

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,37 @@
+package com.stackroute.domain;
+/*
+Displays actor information for respective movies.
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Movie {
+    @Autowired
+    private Actor actor;
+
+    public Movie() {
+
+    }
+
+    @Autowired
+    public Movie(Actor actor) {
+        this.actor = actor;
+        System.out.println("Constructor Called");
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+        System.out.println("Setter Method Called");
+    }
+
+    public void displayInformation() {
+        actor.display();
+    }
+
+    public void initMethod() {
+        actor = new Actor("meghana", "Female", 22);
+    }
+}
